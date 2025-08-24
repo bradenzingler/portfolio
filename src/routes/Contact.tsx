e6ef9accd7db7dc0c5c52e4531b51a953230eb63
Braden Zingler <bradenzingler@gmail.com>
2025-08-24 12:25:26 -0500

    Fix css layout issue

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -2,7 +2,7 @@ import ContactForm from "../components/contact-form/ContactForm";
 
 export default function Contact() {
 	return (
-		<main className="main">
+		<main className="two-cols">
 			<div className="gap-lg col">
 				<h1>Let&apos;s chat</h1>
 				<p>Fill out the form below to send me a message. Please include your contact information.</p>
