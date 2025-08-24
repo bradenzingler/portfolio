f62371591b05ff8a6345ee4ab7f8f7e05c1e2586
Braden Zingler <bradenzingler@gmail.com>
2025-08-24 13:03:58 -0500

    Google analytics already has link clicks built in

--- src/components/header/Link.tsx
+++ src/components/header/Link.tsx
@@ -1,4 +1,3 @@
-import type { MouseEvent } from "react";
 import type { IconType } from "react-icons";
 
 type iconProps = {
@@ -13,22 +12,8 @@ type props = {
 };
 
 export default function Link({ href, title, Icon, iconProps }: props) {
-	const onClick = (e: MouseEvent<HTMLAnchorElement>) => {
-		console.log(e);
-
-		gtag("event", "click", { 
-            event_category: "engagement",
-            link_name: title 
-        });
-	};
-
 	return (
-		<a
-			onClick={onClick}
-			href={href}
-			title={title}
-			rel="noopener noreferrer"
-		>
+		<a href={href} title={title} rel="noopener noreferrer">
 			{Icon && <Icon size={35} {...iconProps} />}
 		</a>
 	);
