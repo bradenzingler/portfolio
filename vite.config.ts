81e24b87830bec45f6d737fdf18dd1cfb69d81a5
Braden Zingler <bradenzingler@gmail.com>
2025-08-23 19:21:37 -0500

    Font optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
-})
+	plugins: [react(), cloudflare()],
+	build: {
+		target: "es2020",
+		minify: "terser",
+		sourcemap: false,
+		reportCompressedSize: true,
+		terserOptions: {
+            compress: true,
+            mangle: true,
+		},
+	},
+});
