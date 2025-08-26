531ce2cad5622d106ef5b85158a2d6e40b1f26ab
Braden Zingler <bradenzingler@gmail.com>
2025-08-26 18:38:54 -0500

    Add analytics pushing for ref

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import Header from "./components/header/Header";
 
 export default function App() {
 	return (
-		<main className="two-cols">
-			<Header />
-			<AboutMe />
+		<main className="">
+			<div className="two-cols">
+				<Header />
+				<AboutMe />
+			</div>
 		</main>
 	);
 }
