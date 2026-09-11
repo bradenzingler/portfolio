4fbf2ced21e7b5a4d12406d3312be0bfd24bd615
Braden Zingler <bradenzingler@gmail.com>
2026-09-11 18:41:01 -0500

    Update profile pic

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,11 +5,11 @@ export default function Header() {
 
 	return (
 		<header>
-			<img id="portrait" src="/portrait.webp" />
+			<img id="portrait" src="/profile_picture.jpg" />
 			<div id="info">
 				<h1>Braden Zingler</h1>
-				<h2>Software & Computer Engineer</h2>
-				<h3>Madison, WI</h3>
+				<h2>Battery Firmware Engineer at Milwaukee Tool</h2>
+				<h3>Milwaukee, WI</h3>
 			</div>
 			<Links />
 		</header>
